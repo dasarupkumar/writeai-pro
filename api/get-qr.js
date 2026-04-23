55602b535a4f9b87aeb48478b572c1a98bc1f07e
dasarupkumar <30717568+dasarupkumar@users.noreply.github.com>
2026-04-23 13:26:06 +0530

    Delete api/get-qr.js

--- api/get-qr.js
+++
@@ -1,9 +0,0 @@
-function QR({ amount }) {
-  const [src, setSrc] = useState("");
-  useEffect(() => {
-    setSrc(`/api/get-qr?amount=${amount}&t=${Date.now()}`);
-  }, [amount]);
-  return src
-    ? <img src={src} alt="Scan to pay" width={180} height={180} style={{borderRadius:8,display:"block"}}/>
-    : <div style={{width:180,height:180,background:"#eee",borderRadius:8,display:"flex",alignItems:"center",justifyContent:"center",fontSize:12,color:"#999"}}>Loading QR...</div>;
-}
